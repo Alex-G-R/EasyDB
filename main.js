669c8e55b05d737dbb859394ff28c68f0ef1675d
Alex <alex.grabski555@gmail.com>
2023-10-25 19:12:24 +0200

    adding create a new database window and creating a functional menu

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
-const {app, BrowserWindow } = require('electron');
 
-app.whenReady().then(()=>{
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
-    const mainWindow = new BrowserWindow({
+let mainWindow;
+let createDatabaseWindow;
+
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow({
         width: 600,
         height: 400,
-        autoHideMenuBar: true, // hide the menu bar
+        autoHideMenuBar: true,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js') // Path to preload script
         }
     });
 
-
-    // load a webpage
     mainWindow.loadFile('main.html');
-
-    // Set the initial window title
     mainWindow.setTitle('EasyDB ~ Main menu');
+
+    ipcMain.on('hide-window', () => {
+        if (mainWindow) {
+            mainWindow.close();
+        }
+
+        mainWindow.on('closed', e => {
+            mainWindow = null;
+        });
+    });
+
+    ipcMain.on('create-database-window-open', () => {
+        if (true) {
+            createDatabaseWindow = new BrowserWindow({
+                width: 1600,
+                height: 900,
+                autoHideMenuBar: true,
+                webPreferences: {
+                    nodeIntegration: true,
+                    preload: path.join(__dirname, 'preload.js') // Path to preload script
+                }
+            });
+
+            createDatabaseWindow.loadFile('./windows/createDatabase.html');
+            createDatabaseWindow.setTitle('EasyDB ~ Create Database');
+        }
+    });
+
 });
+
