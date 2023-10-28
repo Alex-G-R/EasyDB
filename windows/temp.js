e934971ac0dc359936b38b351f0e0512e3be72cb
Alex <alex.grabski555@gmail.com>
2023-10-28 17:53:44 +0200

    rebuilding the app

---
+++ windows/temp.js
@@ -0,0 +1,112 @@
+
+/* Create temp.html script */
+
+/* Database data */
+let databaseName = 'hello';
+
+const mainH1 = document.getElementById("mainH1");
+
+/* Get the HTML input fields */
+const databaseNameInput = document.getElementById("databaseName");
+
+/* Next button setup */
+const next = document.getElementById("next");
+let nextStatus = 0;
+
+next.addEventListener("click", () => {
+
+    switch (nextStatus) {
+        case 0:
+
+            /* Handle naming the database and moving to the next step */
+            /* Check for the nextStatus number */
+            if (databaseNameInput.value.trim() != "") {
+                databaseName = databaseNameInput.value.trim();
+
+                /* Change the next status number */
+                nextStatus = 1;
+
+                /* Chnage the visible input dields*/
+                databaseNameInput.style.display = "none";
+
+                /* Change the mainH1 text */
+                mainH1.innerText = `Editing ${databaseName}`;
+
+                /* Show the add table button */
+                addTable.style.display = "flex";
+            }
+            break;
+
+        // case 1: // next case
+
+    }
+
+});
+
+/* add table setup */
+const addTable = document.getElementById("add-table");
+
+const newTableContainer = document.getElementById('newTableContainer');
+
+let numberOfTables = 0;
+
+
+addTable.addEventListener('click', () => {
+
+    // Create a container for the table
+    const tableContainer = document.createElement('div');
+    tableContainer.setAttribute('class', 'tableContainer')
+    tableContainer.setAttribute('id', `tableContainer_${numberOfTables}`)
+    newTableContainer.appendChild(tableContainer);
+
+    /* Input field type text - table name */
+    const tableName = document.createElement('input');
+    tableName.setAttribute('id', `tableName_${numberOfTables}`);
+    tableName.setAttribute('class', 'input');
+    tableName.setAttribute('type', 'text');
+    tableName.setAttribute('placeholder', 'Table name: ');
+    tableContainer.appendChild(tableName);
+
+    /* Input field type button - edit */
+    const addColumns = document.createElement('input');
+    addColumns.setAttribute('id', `addColumnsButton_${numberOfTables}`);
+    addColumns.setAttribute('class', 'add-columns-button table-creator-button');
+    addColumns.setAttribute('type', 'button');
+    addColumns.setAttribute('value', 'Add columns');
+    tableContainer.appendChild(addColumns);
+
+    /* Input field type button - delete the table */
+    const deleteTheTable = document.createElement('input');
+    deleteTheTable.setAttribute('id', `deleteTheTableButton_${numberOfTables}`);
+    deleteTheTable.setAttribute('class', 'delete-the-table-button table-creator-button');
+    deleteTheTable.setAttribute('type', 'button');
+    deleteTheTable.setAttribute('value', 'Delete this table');
+    deleteTheTable.setAttribute('data-table-number', numberOfTables)
+    tableContainer.appendChild(deleteTheTable);
+
+    // Attach click event listener to the new delete button
+    deleteTheTable.addEventListener('click', () => {
+        // Get the value of the data-table-number attribute
+        let buttonNumber = deleteTheTable.getAttribute('data-table-number');
+        // Use the buttonNumber to identify the associated tableContainer
+        let tableToDelete = document.getElementById(`tableContainer_${buttonNumber}`);
+
+        if (tableToDelete) {
+            tableToDelete.parentNode.removeChild(tableToDelete);
+        }
+    });
+    
+
+    numberOfTables++;
+});
+
+
+
+
+
+/* Handle the menu button */
+const backToMainMenu = document.getElementById('back-to-menu');
+// hide the current window and show the main menu
+backToMainMenu.addEventListener('click', () => {
+    window.ipcRenderer.send('back-to-main-menu');
+});
